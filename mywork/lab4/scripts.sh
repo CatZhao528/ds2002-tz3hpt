99a6732f2b9404d494a0cc8398bec372a2174b9f
CatZhao528 <110512825+CatZhao528@users.noreply.github.com>
2024-05-03 22:58:43 -0400

    Update scripts.sh

--- mywork/lab4/scripts.sh
+++ mywork/lab4/scripts.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Variables
-bucket_name="bucket-name"
-file_path="/local/path/to/your/image.jpg"
-file_name="image.jpg"
+bucket_name="ds2002-tz3hpt"
+file_path="C:/Users/surface/Desktop/Cat_August_2010-4.jpg"
+file_name="Cat_August_2010-4.jpg"
 
-# Upload the file
+# Upload
 aws s3 cp "$file_path" "s3://$bucket_name/$file_name"
 
 # Make the file public
 aws s3api put-object-acl --bucket $bucket_name --key $file_name --acl public-read
 
-# Generate a presigned URL
 aws s3 presign "s3://$bucket_name/$file_name" --expires-in 604800
